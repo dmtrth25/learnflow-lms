c173e5ba05272ef7115c4ff2afa6b8a8f037cc9f
dmtrth25 <vodolazhchenko25@gmail.com>
2026-04-29 13:58:21 +0300

    init setup

---
+++ src/app/router.tsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div className="p-6 text-2xl font-bold">LearnFlow LMS</div>,
+  },
+])
